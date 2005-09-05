e8550c8d618941315784686e67197f4f07e69bea
Frank Bergmann <frank.bergmann@project-open.com>
2005-09-05 15:28:24 +0000

    - beautified (no changes) core/intranet-company-costs - added im_cost_permissions to cost-procs: Allowing externals   only to view documents

--- www/new.tcl
+++ www/new.tcl
@@ -54,11 +54,24 @@ if {[info exists del_invoice]} {
 }
 
 
-#ns_log notice "**************** $user_id permission [im_permission $user_id view_invoices]**********"
-#if {![im_permission $user_id view_invoices]} {
-#    ad_return_complaint "Insufficient Privileges" "
-#    <li>You don't have sufficient privileges to see this page."    
-#}
+#!!!
+im_company_permissions $user_id $company_id view read write admin
+
+if {!$read && ![im_permission $user_id view_invoices]} {
+    ad_return_complaint "[lang::message::lookup $locale intranet-invoices.lt_Insufficient_Privileg]" "
+    <li>[lang::message::lookup $locale intranet-invoices.lt_You_have_insufficient_1]<BR>
+    [lang::message::lookup $locale intranet-invoices.lt_Please_contact_your_s]"
+    return
+}
+
+if {![im_permission $user_id view_invoices]} {
+    ad_return_complaint "Insufficient Privileges" "
+    <li>You don't have sufficient privileges to see this page."    
+}
+
+
+
+
 
 set return_url [im_url_with_query]
 set todays_date [db_string get_today "select to_char(sysdate,'YYYY-MM-DD') from dual"]
