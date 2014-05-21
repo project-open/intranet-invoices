272fe1e68bc204f0705757e55393f5ba742671d6
Frank Bergmann <frank.bergmann@project-open.com>
2014-05-21 20:14:08 +0000

    - beautified

--- tcl/intranet-invoices-procs.tcl
+++ tcl/intranet-invoices-procs.tcl
@@ -730,6 +730,7 @@ ad_proc -public im_invoice_permissions {
     Fill the "by-reference" variables read, write and admin
     with the permissions of $current_user_id on $user_id
 } {
+    im_security_alert_check_integer -location "im_invoice_permissions: user_id" -value $user_id
     upvar $view_var view
     upvar $read_var read
     upvar $write_var write
@@ -745,7 +746,7 @@ ad_proc -public im_invoice_permissions {
 
     # Admins and creators can do everything
     set user_is_admin_p [im_is_user_site_wide_or_intranet_admin $current_user_id]
-    set creation_user_id [util_memoize "db_string creator {select creation_user from acs_objects where object_id = $user_id} -default 0"]
+    set creation_user_id [util_memoize [list db_string creator "select creation_user from acs_objects where object_id = $user_id" -default 0]]
     if {$user_is_admin_p || $current_user_id == $creation_user_id} {
         set view 1
         set read 1
