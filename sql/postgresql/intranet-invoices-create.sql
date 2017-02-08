4d3015c1141894584c78977b7486e66b4631a2d9
Frank Bergmann <frank.bergmann@project-open.com>
2017-02-08 09:49:07 +0000

    - Fixed creation scripts for OpenACS Installer

--- sql/postgresql/intranet-invoices-create.sql
+++ sql/postgresql/intranet-invoices-create.sql
@@ -266,6 +266,46 @@ select im_priv_create('add_invoices','P/O Admins');
 select im_priv_create('add_invoices','Senior Managers');
 
 
+
+
+-----------------------------------------------------------
+-- TSearch2 Full-Text Search Engine
+
+-- We are going for Invoice instead of im_costs, because of
+-- performance reasons. There many be many cost items, but
+-- they don't usually interest us very much.
+
+insert into im_search_object_types values (4,'im_invoice',1);
+
+create or replace function im_invoice_tsearch ()
+returns trigger as $$
+declare
+	v_string	varchar;
+begin
+	select	coalesce(i.invoice_nr, '') || ' ' ||
+		coalesce(c.cost_nr, '') || ' ' ||
+		coalesce(c.cost_name, '') || ' ' ||
+		coalesce(c.description, '') || ' ' ||
+		coalesce(c.note, '')
+	into
+		v_string
+	from
+		im_invoices i,
+		im_costs c
+	where	
+		i.invoice_id = c.cost_id
+		and i.invoice_id = new.invoice_id;
+
+	perform im_search_update(new.invoice_id, 'im_invoice', new.invoice_id, v_string);
+	return new;
+end;$$ language 'plpgsql';
+
+CREATE TRIGGER im_invoices_tsearch_tr
+AFTER INSERT or UPDATE ON im_invoices
+FOR EACH ROW EXECUTE PROCEDURE im_invoice_tsearch();
+
+
+
 ------------------------------------------------------
 -- Views to Business Objects
 --
