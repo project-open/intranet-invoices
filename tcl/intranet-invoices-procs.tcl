23ad1458194a7602a69229cd8630eead6e3822bb
Frank Bergmann <frank.bergmann@project-open.com>
2018-01-25 14:44:03 +0000

    - Fixed generation of invoice_nr for cost types   with double digit prefix

--- tcl/intranet-invoices-procs.tcl
+++ tcl/intranet-invoices-procs.tcl
@@ -172,11 +172,19 @@ ad_proc -public im_next_invoice_nr {
 	    3704 { set prefix "B" }
 	    3706 { set prefix "P" }
 	    3718 { set prefix "T" }
-	    3720 { set prefix "E" }
+	    3720 { set prefix "EI" }
+	    3722 { set prefix "EB" }
 	    3724 { set prefix "D" }
 	    3730 { set prefix "II" }
 	    3732 { set prefix "IQ" }
 	    3734 { set prefix "R" }
+
+	    3737 { set prefix "T" }
+	    3738 { set prefix "L" }
+	    3739 { set prefix "CR" }
+	    3740 { set prefix "DP" }
+	    3741 { set prefix "CE" }
+
 	    default { set prefix "" }
 	}
     }
@@ -193,7 +201,7 @@ ad_proc -public im_next_invoice_nr {
 
     set prefix_where ""
     if {$prefix_len} {
-	set prefix_where "and substr(invoice_nr, 1, 1) = :prefix"
+	set prefix_where "and substr(invoice_nr, 1, $prefix_len) = :prefix"
     }
 
     set sql "
