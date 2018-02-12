9e148bf793f23147f98a981d4b3b631965e78a9b
Frank Bergmann <frank.bergmann@project-open.com>
2018-02-12 15:05:43 +0000

    - Fixed issue with cost item not related to project(?)

--- www/new.tcl
+++ www/new.tcl
@@ -64,6 +64,16 @@ if {[info exists del_invoice]} {
     # will dissappear...
     set return_url "/intranet-invoices/list"
     set project_id [db_string pid "select project_id from im_costs where cost_id = :invoice_id" -default 0]
+    if {"" eq $project_id || 0 eq $project_id} {
+	set project_id [db_string pid "
+		select	min(p.project_id)
+		from	acs_rels r,
+			im_projects p
+		where	r.object_id_two = :invoice_id and
+			r.object_id_one = p.project_id
+        " -default 0]
+    }
+
     if {"" != $project_id && 0 != $project_id} {
 	set view_name "finance"
 	set return_url [export_vars -base "/intranet/projects/view" {project_id view_name}] 
